67f0a3c7b726aba352e01e672f2735d3eb3a9b3e
WesleyGomesDev <wesleydev.gomes@gmail.com>
2024-09-30 21:20:44 -0300

    Adicionar arquivos de projeto.

---
+++ Cadastro/FrmFuncionario.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace PDV.Cadstro
+{
+    partial class FrmFuncionario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmFuncionario));
+            this.lblnome = new System.Windows.Forms.Label();
+            this.lblcpf = new System.Windows.Forms.Label();
+            this.lblTelefone = new System.Windows.Forms.Label();
+            this.lblEndereço = new System.Windows.Forms.Label();
+            this.lblCargo = new System.Windows.Forms.Label();
+            this.lblFoto = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtTelefone = new System.Windows.Forms.TextBox();
+            this.txtEndereco = new System.Windows.Forms.TextBox();
+            this.comboCargo = new System.Windows.Forms.ComboBox();
+            this.txtCPF = new System.Windows.Forms.TextBox();
+            this.IMG = new System.Windows.Forms.PictureBox();
+            this.dataGrid = new System.Windows.Forms.DataGridView();
+            this.btnNovo = new System.Windows.Forms.Button();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.IMG)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblnome
+            // 
+            this.lblnome.AutoSize = true;
+            this.lblnome.Location = new System.Drawing.Point(39, 46);
+            this.lblnome.Name = "lblnome";
+            this.lblnome.Size = new System.Drawing.Size(35, 13);
+            this.lblnome.TabIndex = 0;
+            this.lblnome.Text = "Nome";
+            // 
+            // lblcpf
+            // 
+            this.lblcpf.AutoSize = true;
+            this.lblcpf.Location = new System.Drawing.Point(347, 42);
+            this.lblcpf.Name = "lblcpf";
+            this.lblcpf.Size = new System.Drawing.Size(27, 13);
+            this.lblcpf.TabIndex = 1;
+            this.lblcpf.Text = "CPF";
+            // 
+            // lblTelefone
+            // 
+            this.lblTelefone.AutoSize = true;
+            this.lblTelefone.Location = new System.Drawing.Point(39, 76);
+            this.lblTelefone.Name = "lblTelefone";
+            this.lblTelefone.Size = new System.Drawing.Size(49, 13);
+            this.lblTelefone.TabIndex = 2;
+            this.lblTelefone.Text = "Telefone";
+            // 
+            // lblEndereço
+            // 
+            this.lblEndereço.AutoSize = true;
+            this.lblEndereço.Location = new System.Drawing.Point(39, 104);
+            this.lblEndereço.Name = "lblEndereço";
+            this.lblEndereço.Size = new System.Drawing.Size(53, 13);
+            this.lblEndereço.TabIndex = 3;
+            this.lblEndereço.Text = "Endereço";
+            // 
+            // lblCargo
+            // 
+            this.lblCargo.AutoSize = true;
+            this.lblCargo.Location = new System.Drawing.Point(347, 84);
+            this.lblCargo.Name = "lblCargo";
+            this.lblCargo.Size = new System.Drawing.Size(35, 13);
+            this.lblCargo.TabIndex = 4;
+            this.lblCargo.Text = "Cargo";
+            // 
+            // lblFoto
+            // 
+            this.lblFoto.AutoSize = true;
+            this.lblFoto.Location = new System.Drawing.Point(560, 38);
+            this.lblFoto.Name = "lblFoto";
+            this.lblFoto.Size = new System.Drawing.Size(28, 13);
+            this.lblFoto.TabIndex = 5;
+            this.lblFoto.Text = "Foto";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(105, 39);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(148, 20);
+            this.txtNome.TabIndex = 6;
+            // 
+            // txtTelefone
+            // 
+            this.txtTelefone.Location = new System.Drawing.Point(105, 73);
+            this.txtTelefone.Name = "txtTelefone";
+            this.txtTelefone.Size = new System.Drawing.Size(65, 20);
+            this.txtTelefone.TabIndex = 8;
+            // 
+            // txtEndereco
+            // 
+            this.txtEndereco.Location = new System.Drawing.Point(105, 104);
+            this.txtEndereco.Name = "txtEndereco";
+            this.txtEndereco.Size = new System.Drawing.Size(134, 20);
+            this.txtEndereco.TabIndex = 9;
+            // 
+            // comboCargo
+            // 
+            this.comboCargo.FormattingEnabled = true;
+            this.comboCargo.Items.AddRange(new object[] {
+            "Gerente",
+            "Atendente"});
+            this.comboCargo.Location = new System.Drawing.Point(388, 84);
+            this.comboCargo.Name = "comboCargo";
+            this.comboCargo.Size = new System.Drawing.Size(121, 21);
+            this.comboCargo.TabIndex = 10;
+            // 
+            // txtCPF
+            // 
+            this.txtCPF.Location = new System.Drawing.Point(388, 39);
+            this.txtCPF.Name = "txtCPF";
+            this.txtCPF.Size = new System.Drawing.Size(100, 20);
+            this.txtCPF.TabIndex = 11;
+            // 
+            // IMG
+            // 
+            this.IMG.Location = new System.Drawing.Point(608, 12);
+            this.IMG.Name = "IMG";
+            this.IMG.Size = new System.Drawing.Size(180, 138);
+            this.IMG.TabIndex = 12;
+            this.IMG.TabStop = false;
+            // 
+            // dataGrid
+            // 
+            this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid.Location = new System.Drawing.Point(12, 169);
+            this.dataGrid.Name = "dataGrid";
+            this.dataGrid.Size = new System.Drawing.Size(776, 190);
+            this.dataGrid.TabIndex = 13;
+            // 
+            // btnNovo
+            // 
+            this.btnNovo.Location = new System.Drawing.Point(17, 365);
+            this.btnNovo.Name = "btnNovo";
+            this.btnNovo.Size = new System.Drawing.Size(75, 23);
+            this.btnNovo.TabIndex = 14;
+            this.btnNovo.Text = "Novo";
+            this.btnNovo.UseVisualStyleBackColor = true;
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(102, 365);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(75, 23);
+            this.btnSalvar.TabIndex = 14;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Location = new System.Drawing.Point(183, 365);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(75, 23);
+            this.btnEditar.TabIndex = 14;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Location = new System.Drawing.Point(268, 365);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(75, 23);
+            this.btnExcluir.TabIndex = 14;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            // 
+            // FrmFuncionario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Khaki;
+            this.ClientSize = new System.Drawing.Size(807, 417);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnNovo);
+            this.Controls.Add(this.dataGrid);
+            this.Controls.Add(this.IMG);
+            this.Controls.Add(this.txtCPF);
+            this.Controls.Add(this.comboCargo);
+            this.Controls.Add(this.txtEndereco);
+            this.Controls.Add(this.txtTelefone);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.lblFoto);
+            this.Controls.Add(this.lblCargo);
+            this.Controls.Add(this.lblEndereço);
+            this.Controls.Add(this.lblTelefone);
+            this.Controls.Add(this.lblcpf);
+            this.Controls.Add(this.lblnome);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmFuncionario";
+            this.Text = "Cadastro Funcionario";
+            this.Load += new System.EventHandler(this.FrmFuncionario_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.IMG)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblnome;
+        private System.Windows.Forms.Label lblcpf;
+        private System.Windows.Forms.Label lblTelefone;
+        private System.Windows.Forms.Label lblEndereço;
+        private System.Windows.Forms.Label lblCargo;
+        private System.Windows.Forms.Label lblFoto;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtTelefone;
+        private System.Windows.Forms.TextBox txtEndereco;
+        private System.Windows.Forms.ComboBox comboCargo;
+        private System.Windows.Forms.TextBox txtCPF;
+        private System.Windows.Forms.PictureBox IMG;
+        private System.Windows.Forms.DataGridView dataGrid;
+        private System.Windows.Forms.Button btnNovo;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnExcluir;
+    }
+}
---
+++ FrmPrincipal.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace PDV
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPrincipal));
+            this.MenuPrincipal = new System.Windows.Forms.MenuStrip();
+            this.MenuCadastro = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuFuncionario = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuUsuario = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuCargos = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuFornecedor = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuProdutos = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuCadastros = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuEstoque = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuMovimentacoes = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuFluxoCaixa = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuLancarVendas = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuEntradasSaidas = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuDespesas = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuRelatorio = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuProduto = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuVendas = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuMovimentosRel = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuEntradaSaidaRel = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuDespesasRel = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuSair = new System.Windows.Forms.ToolStripMenuItem();
+            this.img04 = new System.Windows.Forms.PictureBox();
+            this.img03 = new System.Windows.Forms.PictureBox();
+            this.img02 = new System.Windows.Forms.PictureBox();
+            this.img01 = new System.Windows.Forms.PictureBox();
+            this.MenuPrincipal.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.img04)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img03)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img02)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img01)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MenuPrincipal
+            // 
+            this.MenuPrincipal.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuCadastro,
+            this.MenuProdutos,
+            this.MenuMovimentacoes,
+            this.MenuRelatorio,
+            this.MenuSair});
+            this.MenuPrincipal.Location = new System.Drawing.Point(0, 0);
+            this.MenuPrincipal.Name = "MenuPrincipal";
+            this.MenuPrincipal.Size = new System.Drawing.Size(914, 24);
+            this.MenuPrincipal.TabIndex = 0;
+            this.MenuPrincipal.Text = "menuStrip1";
+            // 
+            // MenuCadastro
+            // 
+            this.MenuCadastro.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuFuncionario,
+            this.MenuUsuario,
+            this.MenuCargos,
+            this.MenuFornecedor});
+            this.MenuCadastro.Name = "MenuCadastro";
+            this.MenuCadastro.Size = new System.Drawing.Size(66, 20);
+            this.MenuCadastro.Text = "Cadastro";
+            // 
+            // MenuFuncionario
+            // 
+            this.MenuFuncionario.Name = "MenuFuncionario";
+            this.MenuFuncionario.Size = new System.Drawing.Size(142, 22);
+            this.MenuFuncionario.Text = "Funcionários";
+            this.MenuFuncionario.Click += new System.EventHandler(this.MenuFuncionario_Click);
+            // 
+            // MenuUsuario
+            // 
+            this.MenuUsuario.Name = "MenuUsuario";
+            this.MenuUsuario.Size = new System.Drawing.Size(142, 22);
+            this.MenuUsuario.Text = "Usuários";
+            // 
+            // MenuCargos
+            // 
+            this.MenuCargos.Name = "MenuCargos";
+            this.MenuCargos.Size = new System.Drawing.Size(142, 22);
+            this.MenuCargos.Text = "Cargos";
+            // 
+            // MenuFornecedor
+            // 
+            this.MenuFornecedor.Name = "MenuFornecedor";
+            this.MenuFornecedor.Size = new System.Drawing.Size(142, 22);
+            this.MenuFornecedor.Text = "Fornecedor";
+            // 
+            // MenuProdutos
+            // 
+            this.MenuProdutos.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuCadastros,
+            this.MenuEstoque});
+            this.MenuProdutos.Name = "MenuProdutos";
+            this.MenuProdutos.Size = new System.Drawing.Size(67, 20);
+            this.MenuProdutos.Text = "Produtos";
+            // 
+            // MenuCadastros
+            // 
+            this.MenuCadastros.Name = "MenuCadastros";
+            this.MenuCadastros.Size = new System.Drawing.Size(121, 22);
+            this.MenuCadastros.Text = "Cadastro";
+            // 
+            // MenuEstoque
+            // 
+            this.MenuEstoque.Name = "MenuEstoque";
+            this.MenuEstoque.Size = new System.Drawing.Size(121, 22);
+            this.MenuEstoque.Text = "Estoque";
+            // 
+            // MenuMovimentacoes
+            // 
+            this.MenuMovimentacoes.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuFluxoCaixa,
+            this.MenuLancarVendas,
+            this.MenuEntradasSaidas,
+            this.MenuDespesas});
+            this.MenuMovimentacoes.Name = "MenuMovimentacoes";
+            this.MenuMovimentacoes.Size = new System.Drawing.Size(104, 20);
+            this.MenuMovimentacoes.Text = "Movimentações";
+            // 
+            // MenuFluxoCaixa
+            // 
+            this.MenuFluxoCaixa.Name = "MenuFluxoCaixa";
+            this.MenuFluxoCaixa.Size = new System.Drawing.Size(163, 22);
+            this.MenuFluxoCaixa.Text = "Fluxo de caixa";
+            // 
+            // MenuLancarVendas
+            // 
+            this.MenuLancarVendas.Name = "MenuLancarVendas";
+            this.MenuLancarVendas.Size = new System.Drawing.Size(163, 22);
+            this.MenuLancarVendas.Text = "Lançar Vendas";
+            // 
+            // MenuEntradasSaidas
+            // 
+            this.MenuEntradasSaidas.Name = "MenuEntradasSaidas";
+            this.MenuEntradasSaidas.Size = new System.Drawing.Size(163, 22);
+            this.MenuEntradasSaidas.Text = "Entradas / Saídas";
+            // 
+            // MenuDespesas
+            // 
+            this.MenuDespesas.Name = "MenuDespesas";
+            this.MenuDespesas.Size = new System.Drawing.Size(163, 22);
+            this.MenuDespesas.Text = "Despesas";
+            // 
+            // MenuRelatorio
+            // 
+            this.MenuRelatorio.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuProduto,
+            this.MenuVendas,
+            this.MenuMovimentosRel,
+            this.MenuEntradaSaidaRel,
+            this.MenuDespesasRel});
+            this.MenuRelatorio.Name = "MenuRelatorio";
+            this.MenuRelatorio.Size = new System.Drawing.Size(66, 20);
+            this.MenuRelatorio.Text = "Relatório";
+            // 
+            // MenuProduto
+            // 
+            this.MenuProduto.Name = "MenuProduto";
+            this.MenuProduto.Size = new System.Drawing.Size(163, 22);
+            this.MenuProduto.Text = "Produtos";
+            // 
+            // MenuVendas
+            // 
+            this.MenuVendas.Name = "MenuVendas";
+            this.MenuVendas.Size = new System.Drawing.Size(163, 22);
+            this.MenuVendas.Text = "Vendas";
+            // 
+            // MenuMovimentosRel
+            // 
+            this.MenuMovimentosRel.Name = "MenuMovimentosRel";
+            this.MenuMovimentosRel.Size = new System.Drawing.Size(163, 22);
+            this.MenuMovimentosRel.Text = "Movimento";
+            // 
+            // MenuEntradaSaidaRel
+            // 
+            this.MenuEntradaSaidaRel.Name = "MenuEntradaSaidaRel";
+            this.MenuEntradaSaidaRel.Size = new System.Drawing.Size(163, 22);
+            this.MenuEntradaSaidaRel.Text = "Entradas / Saidas";
+            // 
+            // MenuDespesasRel
+            // 
+            this.MenuDespesasRel.Name = "MenuDespesasRel";
+            this.MenuDespesasRel.Size = new System.Drawing.Size(163, 22);
+            this.MenuDespesasRel.Text = "Despesas";
+            // 
+            // MenuSair
+            // 
+            this.MenuSair.Name = "MenuSair";
+            this.MenuSair.Size = new System.Drawing.Size(38, 20);
+            this.MenuSair.Text = "Sair";
+            this.MenuSair.Click += new System.EventHandler(this.MenuSair_Click);
+            // 
+            // img04
+            // 
+            this.img04.Image = global::PDV.Properties.Resources.loja_online;
+            this.img04.Location = new System.Drawing.Point(464, 270);
+            this.img04.Name = "img04";
+            this.img04.Size = new System.Drawing.Size(179, 177);
+            this.img04.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img04.TabIndex = 1;
+            this.img04.TabStop = false;
+            // 
+            // img03
+            // 
+            this.img03.Image = global::PDV.Properties.Resources.recepcionista;
+            this.img03.Location = new System.Drawing.Point(185, 228);
+            this.img03.Name = "img03";
+            this.img03.Size = new System.Drawing.Size(179, 177);
+            this.img03.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img03.TabIndex = 1;
+            this.img03.TabStop = false;
+            // 
+            // img02
+            // 
+            this.img02.Image = global::PDV.Properties.Resources.bolsa_de_dinheiro1;
+            this.img02.Location = new System.Drawing.Point(464, 87);
+            this.img02.Name = "img02";
+            this.img02.Size = new System.Drawing.Size(179, 177);
+            this.img02.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img02.TabIndex = 1;
+            this.img02.TabStop = false;
+            // 
+            // img01
+            // 
+            this.img01.Image = global::PDV.Properties.Resources.carrinho_de_compras;
+            this.img01.Location = new System.Drawing.Point(186, 45);
+            this.img01.Name = "img01";
+            this.img01.Size = new System.Drawing.Size(179, 177);
+            this.img01.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img01.TabIndex = 1;
+            this.img01.TabStop = false;
+            // 
+            // frmPrincipal
+            // 
+            this.AccessibleDescription = "";
+            this.AccessibleName = "";
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Khaki;
+            this.ClientSize = new System.Drawing.Size(914, 462);
+            this.Controls.Add(this.img04);
+            this.Controls.Add(this.img03);
+            this.Controls.Add(this.img02);
+            this.Controls.Add(this.img01);
+            this.Controls.Add(this.MenuPrincipal);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.MenuPrincipal;
+            this.Name = "frmPrincipal";
+            this.Text = "Principal";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.MenuPrincipal.ResumeLayout(false);
+            this.MenuPrincipal.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.img04)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img03)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img02)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img01)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip MenuPrincipal;
+        private System.Windows.Forms.ToolStripMenuItem MenuCadastro;
+        private System.Windows.Forms.ToolStripMenuItem MenuProdutos;
+        private System.Windows.Forms.ToolStripMenuItem MenuMovimentacoes;
+        private System.Windows.Forms.ToolStripMenuItem MenuRelatorio;
+        private System.Windows.Forms.ToolStripMenuItem MenuSair;
+        private System.Windows.Forms.PictureBox img01;
+        private System.Windows.Forms.PictureBox img02;
+        private System.Windows.Forms.PictureBox img03;
+        private System.Windows.Forms.PictureBox img04;
+        private System.Windows.Forms.ToolStripMenuItem MenuFuncionario;
+        private System.Windows.Forms.ToolStripMenuItem MenuUsuario;
+        private System.Windows.Forms.ToolStripMenuItem MenuCargos;
+        private System.Windows.Forms.ToolStripMenuItem MenuFornecedor;
+        private System.Windows.Forms.ToolStripMenuItem MenuCadastros;
+        private System.Windows.Forms.ToolStripMenuItem MenuEstoque;
+        private System.Windows.Forms.ToolStripMenuItem MenuFluxoCaixa;
+        private System.Windows.Forms.ToolStripMenuItem MenuLancarVendas;
+        private System.Windows.Forms.ToolStripMenuItem MenuEntradasSaidas;
+        private System.Windows.Forms.ToolStripMenuItem MenuDespesas;
+        private System.Windows.Forms.ToolStripMenuItem MenuProduto;
+        private System.Windows.Forms.ToolStripMenuItem MenuVendas;
+        private System.Windows.Forms.ToolStripMenuItem MenuMovimentosRel;
+        private System.Windows.Forms.ToolStripMenuItem MenuEntradaSaidaRel;
+        private System.Windows.Forms.ToolStripMenuItem MenuDespesasRel;
+    }
+}
+
